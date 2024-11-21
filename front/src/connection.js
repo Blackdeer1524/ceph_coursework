a65695d838ed5526f13ea6156327a118dcf4e1d6
Danila Slesarev <blackdeer1524@yandex.ru>
2024-11-21 17:47:41 +0300

    checkpoint

--- front/src/connection.js
+++ front/src/connection.js
@@ -1,50 +1,184 @@
-import { Canvas, Rect } from "fabric"
+import { Canvas, Line, Rect } from "fabric";
 
-export class Node {
-    static width = 100;
-    static leftConnectionOffset = 5
-    static widhtWithConnections = Node.width + Node.leftConnectionOffset
-    static height = 60;
+export class Bucket {
+  static width = 100;
+  static height = 60;
 
-    constructor(posX, posY, canvas) {
-        this.posX = posX + OSD.leftConnectionOffset
-        this.posY = posY
-        this.canvas = canvas
+  /**
+   * @param {number} posX
+   * @param {number} posY
+   * @param {Canvas} canvas
+   */
+  constructor(posX, posY, canvas) {
+    this.posX = posX;
+    this.posY = posY;
+    console.log("bucket", [posX, posY]);
+    this.canvas = canvas;
 
-        this.drawnObj = new Rect({
-            top: posY + OSD.leftConnectionOffset,
-            left: posX,
-            width: Node.width,
-            height: Node.height,
-        })
-        this.canvas.add(this.drawnObj)
+    this.children = [];
+
+    this.drawnObj = new Rect({
+      top: posY,
+      left: posX,
+      width: Bucket.width,
+      height: Bucket.height,
+      fill: "green",
+    });
+    this.canvas.add(this.drawnObj);
+  }
+
+  /**
+   * @param {Bucket} other
+   */
+  connectParentBucket(other) {
+    this.children.push(other);
+    const myMidpoint = this.posX + Bucket.width / 2;
+    const otherMidpoint = other.posX + Bucket.width / 2;
+    const spaceBetween = this.posY - (other.posY + Bucket.height);
+
+    const start = new Line(
+      [myMidpoint, this.posY, myMidpoint, this.posY - spaceBetween / 2],
+      {
+        stroke: "green",
+      }
+    );
+
+    const middle = new Line(
+      [
+        myMidpoint,
+        this.posY - spaceBetween / 2,
+        otherMidpoint,
+        this.posY - spaceBetween / 2,
+      ],
+      {
+        stroke: "green",
+      }
+    );
+
+    const end = new Line(
+      [
+        otherMidpoint,
+        this.posY - spaceBetween / 2,
+        otherMidpoint,
+        this.posY - spaceBetween,
+      ],
+      {
+        stroke: "green",
+      }
+    );
+
+    this.canvas.add(start);
+    this.canvas.add(middle);
+    this.canvas.add(end);
+
+    // this.childConnections.push(start);
+    // this.childConnections.push(middle);
+    // this.childConnections.push(end);
+  }
+}
+
+const PGBoxHeight = 20;
+const PGBoxGap = 3;
+
+class ConnectionAllocator {
+  /**
+   * 
+   * @param {number} max 
+   */
+  constructor(max) {
+    this.is_allocated = [];
+    for (i = 0; i < max; ++i) {
+      this.is_allocated.push(false);
     }
+  }
+  
+  alloc() {
 
-    connect(otherNode) {
-    }
+  }
 }
 
 export class OSD {
-    static width = 100;
-    static leftConnectionOffset = 5
-    static rightConnectionOffset = 25
+  static width = 100;
+  static initHeight = 60;
 
-    static widhtWithConnections = OSD.width + Node.leftConnectionOffset + OSD.rightConnectionOffset
-    static initHeight = 60;
+  constructor(posX, posY, canvas) {
+    console.log("osd", [posX, posY]);
+    this.posX = posX;
+    this.posY = posY;
+    this.canvas = canvas;
 
-    constructor(posX, posY, canvas) {
-        this.posX = posX + OSD.leftConnectionOffset
-        this.posY = posY
-        this.canvas = canvas
+    this.nextOSD = null;
+    this.pgs = new Map();
 
-        this.drawnObj = new Rect({
-            top: posY + OSD.leftConnectionOffset,
-            left: posX,
-            width: OSD.width,
-            height: OSD.initHeight,
-        })
-        this.canvas.add(this.drawnObj)
+    this.drawnObj = new Rect({
+      top: posY,
+      left: posX,
+      width: OSD.width,
+      height: OSD.initHeight,
+      fill: "blue",
+    });
+    this.canvas.add(this.drawnObj);
+  }
+
+  connect(other, pg_id, connectAllocInfo) {}
+
+  /**
+   *
+   * @param {OSD} other
+   */
+  addNext(other) {
+    this.nextOSD = other;
+  }
+
+  /**
+   * @param {number} id
+   */
+  addPG(id) {
+    let top = this.posY + PGBoxGap;
+    if (this.pgs.length > 0) {
+      top = this.pgs[this.pgs.length - 1].top + PGBoxHeight + PGBoxGap;
+      prin;
     }
+
+    let newPgBox = new Rect({
+      top: top,
+      left: this.posX + PGBoxGap,
+      width: OSD.width - 2 * PGBoxGap,
+      height: PGBoxHeight,
+      fill: "pink",
+    });
+    this.pgs.push(newPgBox);
+    this.redraw(this.posY);
+  }
+
+  /**
+   * @param {number} newY
+   */
+  redraw(newY) {
+    this.canvas.remove(this.drawnObj);
+    this.pgs.forEach((c) => {
+      this.canvas.remove(c);
+      c.top += newY - this.posY;
+    });
+    const newHeight = Math.max(
+      OSD.initHeight,
+      this.pgs.length * PGBoxHeight + (this.pgs.length + 1) * 3
+    );
+    this.posY = newY;
+    this.drawnObj = new Rect({
+      top: this.posY,
+      left: this.posX,
+      width: OSD.width,
+      height: newHeight,
+      fill: "blue",
+    });
+    this.canvas.add(this.drawnObj);
+    this.pgs.forEach((c) => {
+      this.canvas.add(c);
+    });
+
+    this.nextOSD?.redraw(this.posY + newHeight + STEP_Y_BETWEEN);
+  }
 }
 
 /**
@@ -55,69 +189,92 @@ export class OSD {
 
 /**
  * @typedef {Object} BucketDesc
- * @property {string} name 
+ * @property {string} name
  * @property {"bucket"} type
  * @property {number} children_width
  * @property {BucketDesc[] | OSDDesc[]} children
  */
 
-
-const STEP_X_BETWEEN = 5
-const STEP_Y_BETWEEN = 5
+const SPACE_BETWEEN_OSD_COLS = 60;
+const STEP_Y_BETWEEN = 40;
 
 /**
- * @param {BucketDesc} hierarchy 
+ * @param {BucketDesc} root
  * @returns {number}
  */
-function determineWidth(hierarchy) {
-    hierarchy.children_width = 0
-    if (hierarchy.children[0].type == "osd") {
-        hierarchy.children_width = OSD.widhtWithConnections
-        return OSD.widhtWithConnections
-    }
-    for (let child of hierarchy.children) {
-        hierarchy.children_width += determineWidth(child)
-        hierarchy.children_width += STEP_X_BETWEEN
-    }
-    hierarchy.children_width -= STEP_X_BETWEEN
-    return hierarchy.children_width
+function determineWidth(root) {
+  root.children_width = 0;
+  if (root.children[0].type == "osd") {
+    root.children_width = OSD.width;
+    return root.children_width;
+  }
+  root.children_width = 0;
+  for (let child of root.children) {
+    root.children_width += determineWidth(child);
+    root.children_width += SPACE_BETWEEN_OSD_COLS;
+  }
+  root.children_width -= SPACE_BETWEEN_OSD_COLS;
+  return root.children_width;
 }
 
 /**
- * @param {BucketDesc | OSDDesc} hierarchy 
+ * @typedef {Object} HierarchyInfo
+ * @property {Map<string, OSD>} name2osd
+ * @property {OSD[]} lastColOSD
+ */
+
+/**
+ * @param {Node | null} parent
+ * @param {BucketDesc} root
  * @param {[number, number]} pos
  * @param {Canvas} canvas
+ * @returns {HierarchyInfo}
  */
-export function drawHierarchy(hierarchy, pos, canvas) {
-    determineWidth(hierarchy)
-    if (hierarchy.type == "bucket") {
-        let n = new Node(pos[0], pos[1], canvas)
+export function drawHierarchy(parent, root, pos, canvas) {
+  determineWidth(root);
 
-        let spaceAvailable = hierarchy.children_width
-        if (hierarchy.children[0].type == "osd") {
-            spaceAvailable = 0;
-        }
+  const [rootX, rootY] = pos;
 
-        hierarchy.children.forEach((c) => {
-            if (c.type == "osd") {
-                spaceAvailable -= OSD.widhtWithConnections
-            } else {
-                spaceAvailable -= Node.width
-            }
-        })
+  let n = new Bucket(rootX, rootY, canvas);
+  if (parent !== null) {
+    n.connectParentBucket(parent);
+  }
 
+  let m = new Map();
+  let heightMap = [];
+  let leftBound = rootX + Bucket.width / 2 - root.children_width / 2;
 
+  if (root.children[0].type == "osd") {
+    let childY = pos[1] + Bucket.height + STEP_Y_BETWEEN;
+    let prevOSD = null;
 
-        childY = pos[1] + n.height + STEP_Y_BETWEEN
-        for (let child of hierarchy.children) {
-            drawHierarchy(child,)
-
-        }
+    let osd = undefined;
+    for (let child of root.children) {
+      osd = new OSD(rootX, childY, canvas);
+      prevOSD?.addNext(osd);
+      prevOSD = osd;
+      m.set(child.name, osd);
+      childY += OSD.initHeight + STEP_Y_BETWEEN;
     }
-
-
-
-
-
-
+    return { name2osd: m, lastColOSD: [osd] };
+  } else {
+    const childY = pos[1] + Bucket.height + STEP_Y_BETWEEN;
+    for (let child of root.children) {
+      let indent = (child.children_width - Bucket.width) / 2;
+      let { name2osd: subtreeM, lastColOSD: subtreeHeightMap } = drawHierarchy(
+        n,
+        child,
+        [leftBound + indent, childY],
+        canvas
+      );
+      subtreeM.forEach((value, key) => {
+        m.set(key, value);
+      });
+      subtreeHeightMap.forEach((i) => {
+        heightMap.push(i);
+      });
+      leftBound += child.children_width + SPACE_BETWEEN_OSD_COLS;
+    }
+    return { name2osd: m, lastColOSD: heightMap };
+  }
 }
--- front/src/main.js
+++ front/src/main.js
@@ -1,24 +1,104 @@
-import './style.css'
-import { Rect, Line } from 'fabric';
-import { mapCanvas } from './mapCanvas';
+import "./style.css";
+import { Rect, Line } from "fabric";
+import { mapCanvas } from "./mapCanvas";
+import { drawHierarchy } from "./connection";
 
-import { Node } from './connection';
+import { Bucket } from "./connection";
 
-const rect = new Rect({
-  top: 100,
-  left: 100,
-  width: 60,
-  height: 70,
-  fill: 'red',
-});
-mapCanvas.add(rect);
-mapCanvas.add(new Line([50, 100, 200, 200], {
-  left: 170,
-  top: 150,
-  stroke: 'red'
-}));
+/** @type BucketDesc */
+const h = {
+  name: "root",
+  type: "bucket",
+  children: [
+    {
+      name: "rack-1",
+      type: "bucket",
+      children: [
+        {
+          name: "osd.1",
+          type: "osd",
+        },
+        {
+          name: "osd.2",
+          type: "osd",
+        },
+      ],
+    },
+    {
+      name: "rack-2",
+      type: "bucket",
+      children: [
+        {
+          name: "osd.3",
+          type: "osd",
+        },
+        {
+          name: "osd.4",
+          type: "osd",
+        },
+        {
+          name: "osd.5",
+          type: "osd",
+        },
+        {
+          name: "osd.10",
+          type: "osd",
+        },
+      ],
+    },
+    {
+      name: "rack-3",
+      type: "bucket",
+      children: [
+        {
+          name: "row-1",
+          type: "bucket",
+          children: [
+            {
+              name: "osd.6",
+              type: "osd",
+            },
+            {
+              name: "osd.7",
+              type: "osd",
+            },
+          ],
+        },
+        {
+          name: "row-2",
+          type: "bucket",
+          children: [
+            {
+              name: "osd.8",
+              type: "osd",
+            },
+            {
+              name: "osd.9",
+              type: "osd",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
 
-new Node(110, 0, mapCanvas)
+let res = drawHierarchy(null, h, [0, 0], mapCanvas);
+console.log(res["name2osd"]);
+res.name2osd.get("osd.1").addPG(123)
+res.name2osd.get("osd.1").addPG(124)
+res.name2osd.get("osd.1").addPG(125)
+res.name2osd.get("osd.1").addPG(127)
+
+res.name2osd.get("osd.5").addPG(127)
+res.name2osd.get("osd.5").addPG(128)
+res.name2osd.get("osd.5").addPG(129)
+res.name2osd.get("osd.5").addPG(131)
+
+res.name2osd.get("osd.3").addPG(127)
+res.name2osd.get("osd.3").addPG(128)
+res.name2osd.get("osd.3").addPG(129)
+res.name2osd.get("osd.3").addPG(131)
 
 
 // https://stackoverflow.com/a/35453052
